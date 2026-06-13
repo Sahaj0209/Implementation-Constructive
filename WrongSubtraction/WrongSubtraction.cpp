9390c7fb918d5fcc39c4427f7db6795de773c5b9
Sahaj0209 <sahajjaiswal71@gmail.com>
2026-06-14 00:59:53 +0530

    2

---
+++ WrongSubtraction/WrongSubtraction.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    for(int i =0;i<k;i++){
+        int lastDigit=n%10;
+        if(lastDigit!=0) n=n-1;
+        else if(lastDigit==0) n=n/10;
+    }
+    cout<<n<<endl;
+    return 0;
+}
